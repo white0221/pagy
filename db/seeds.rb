d75dbcd5e8d6cc948efeabee7dd3f9386453b06d
white0221 <whiterm.r0221@gmail.com>
2018-12-03 15:41:17 +0900

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+12.times do |i|
+  post = Post.create(
+    name: "user#{i+1}",
+    content: "これは#{i}回目の投稿です。"
+  )
+end
